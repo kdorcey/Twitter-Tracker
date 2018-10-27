a07e1fc072d2a9bcab40935497a4eb53675f7ee3
Darren Goh <dgoh1@uiowa.edu>
2018-10-27 12:09:39 -0500

    searches gather_tweet test model

--- spec/models/searches_spec.rb
+++ spec/models/searches_spec.rb
@@ -1,10 +1,18 @@
-require 'rspec'
+require 'spec_helper'
 require 'rails_helper'
 
 describe Searches do
 
   it 'should post a tweet' do
-    puts 'AAAAAAAAAAAAAAAAAAAA'
     Searches.test_functionality
   end
+
+  describe 'searching Twitter by keyword' do
+    it 'should call Twitter with hashtag keywords in a time frame' do
+      expect(Searches).to receive(:gather_tweets).with('Ruby', '2018-10-10', '2018-10-25')
+      Searches.gather_tweets('Ruby', '2018-10-10', '2018-10-25')
+    end
+  end
+
+  
 end
