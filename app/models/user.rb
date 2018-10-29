9c4a65848e4b9f309b5e5cecbda816c2025c8f3c
Kyle <35466861+kdorcey@users.noreply.github.com>
2018-10-29 17:05:12 -0500

    improved the model. Will now return integer corresponding with what error should be flashed. Will add the hash of flash messages soon

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,40 +1,34 @@
 class User < ActiveRecord::Base
 
-  def self.create_user!(hashofuser)
+  #*
+  #def self.create_user!(hashofuser)
     ##Make sure input is capable of being cast to a string (though it is a textfield so it shouldn't matter...)
-    if !(hashofuser[:user_name].respond_to?(:to_str) || hashofuser[:email].respond_to?(:to_str))
-      return nil
-    end
+  #  if !(hashofuser[:user_name].respond_to?(:to_str) || hashofuser[:email].respond_to?(:to_str))
+  #    return nil
+  #  end
 
-    hashofuser[:session_token] = SecureRandom.base64
-    User.create!(hashofuser)
-  end
+  #  hashofuser[:session_token] = SecureRandom.base64
+  #  User.create!(hashofuser)
+  #end
 
-  def self.username_is_valid?(username)
-    #Regex from https://stackoverflow.com/questions/12018245/regular-expression-to-validate-username
-    if (username.rindex(/^(?=.{3,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/) == nil)
-      return false
+  def self.create_user(hashofuser)
+    if (hashofuser[:user_name].rindex(/^(?=.{3,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/) == nil)
+      #invalid username
+      return 1
+    elsif (hashofuser[:email].rindex(/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i) == nil)
+      #invalid email
+      return 2
+    elsif(User.exists?(user_name: hashofuser[:user_name]))
+      #username already exists
+      return 3
+    elsif(User.exists?(email: hashofuser[:email]))
+      #email already exists
+      return 4
     else
-      true
+      #valid
+      hashofuser[:session_token] = SecureRandom.base64
+      User.create!(hashofuser)
+      return 0
     end
   end
-
-  def self.username_exists?(username)
-    User.exists?(user_name: username)
-  end
-  def self.email_exists?(email)
-    User.exists?(email: email)
-  end
-
-  def self.email_is_valid?(email)
-    if (email.rindex(/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i) == nil)
-      return false
-    else
-      true
-    end
-  end
-
-
-
-
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,34 +3,31 @@ require 'rails_helper'
 
 describe User do
   describe 'adding a new User' do
-    context 'Test user has been added' do
-      it 'should reject username that matches testUser' do
-        User.create_user!({:user_name => 'testuser',:password => 'password', :verify_password => 'password', :email => 'email'})
-
-        expect(User.create_user!({:user_name => 'testuser',:password => 'password',
-                                  :verify_password => 'password', :email => 'email'})).to be(false)
+    context 'sad paths :(' do
+      it 'should return error code 3 for usernames already in the database' do
+        User.create!({:user_name => 'test',:email=>'test@gmail.com'})
+        user = {:user_name => 'test', :email=> 'moo@gmail.com'}
+        expect(User.create_user(user)).to eq(3)
+      end
+      it 'should return error code 4 for emails already in the database' do
+        User.create!({:user_name => 'test',:email=>'moo@gmail.com'})
+        user = {:user_name => 'different', :email=> 'moo@gmail.com'}
+        expect(User.create_user(user)).to eq(4)
+      end
+      it 'should return error code 2 for invalid emails' do
+        user = {:user_name => 'different', :email=> 'moo 2@gmail.com'}
+        expect(User.create_user(user)).to eq(2)
+      end
+      it 'should return error code 1 for invalid usernames' do
+        user = {:user_name => 'diffe rent', :email=> 'moo2@gmail.com'}
+        expect(User.create_user(user)).to eq(1)
       end
     end
-
-    context 'Testing Formatting' do
-      it 'should reject emails with invalid formatting' do
-        email = 'hot dog22@gmail.com'
-        expect(User.email_is_valid?(email)==false).to be(true)
-      end
-      it 'should approve emails with valid formatting' do
-        email = 'hotdog22@gmail.com'
-        expect(User.email_is_valid?(email)).to be(true)
-      end
-      it 'should reject usernames with invalid formatting' do
-        user_name = 'mrMagoo2 2'
-        expect(User.username_is_valid?(user_name)).to be(false)
-      end
-      it 'should accept usernames with valid formatting' do
-        user_name = 'mrMag.oo22'
-        expect(User.username_is_valid?(user_name)).to be(true)
+    context 'happy path :)' do
+      it 'should create a new table entry for that user' do
+        user = {:user_name => 'testy', :email=> 'moo@gmail.com'}
+        User.create_user(user)
       end
     end
   end
-
-
 end
