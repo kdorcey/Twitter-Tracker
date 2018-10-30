0adf137a8c8316701df116cecc5bae63633ff0fd
Markus Drealan <markus-drealan@uiowa.edu>
2018-10-30 12:03:25 -0500

    added session controller with login, not all before:filters are in place yet.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,33 @@
 class SessionsController < ApplicationController
 
+  def sessions_params
+    params.require(:user).permit(:user_name, :session_token, :password)
+  end
+
   def new
 
   end
 
   def create
 
-    redirect_to :controller => 'users', :action => 'show'
+    user_logged_in = Session.verify_login(sessions_params)
+
+    if user_logged_in.nil?
+      flash[:notice] = "Invalid username/password combination."
+      #Todo: redirect to login path when implemented.
+      #redirect_to login_path
+    else
+      session[:session_token] = user_logged_in
+      flash[:notice]  = "You are logged in as " + sessions_params[:user_name]
+      redirect_to :controller => 'users', :action => 'show'
+    end
   end
 
   def destroy
-
+    @current_user = nil
+    session[:session_token] = nil
+    flash[:notice] = "successful logout"
+    #Todo:: redirect to the main page when implemented.
   end
 
 end
---
+++ app/models/session.rb
@@ -0,0 +1,28 @@
+require 'bcrypt'
+
+class Session < ActiveRecord::Base
+
+  include BCrypt
+
+  def self.verify_login(sessions_params)
+
+    #If successful login, return session token ID. else return nil.
+    #
+    if User.exists?(user_name: sessions_params[:user_name]) ##make sure user exists.
+
+      user_email_and_session = User.where(user_name: sessions_params[:user_name]).pluck(:session_token, :password)
+      user_email_and_session.flatten!
+
+      rehashed_password = BCrypt::Password.new(user_email_and_session[1])
+
+      if sessions_params[:password] == rehashed_password
+        return user_email_and_session[0]
+      else
+        return nil
+      end
+    end
+    return nil
+  end
+
+
+end
