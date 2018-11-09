6259535514c9cdc2e366493ef6f512fdb84ecfed
Kyle <35466861+kdorcey@users.noreply.github.com>
2018-11-09 10:57:36 -0600

    resolving conflict

--- spec/models/searches_spec.rb
+++ spec/models/searches_spec.rb
@@ -1,3 +1,4 @@
+<<<<<<< .merge_file_a01584
 require 'spec_helper'
 require 'rails_helper'
 
@@ -17,3 +18,23 @@ describe Searches do
   
 end
 
+=======
+require 'spec_helper'
+require 'rails_helper'
+
+describe Searches do
+
+  it 'should post a tweet' do
+    Searches.test_functionality
+  end
+
+  describe 'searching Twitter by keyword' do
+    it 'should call Twitter with hashtag keywords in a time frame' do
+      expect(Searches).to receive(:gather_tweets).with('Ruby', '2018-10-10', '2018-10-25')
+      Searches.gather_tweets('Ruby', '2018-10-10', '2018-10-25')
+    end
+  end
+
+  
+end
+>>>>>>> .merge_file_a16096
