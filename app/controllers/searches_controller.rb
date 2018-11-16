49d1da598bbf74acf2de883bfb2e755d21893809
Kyle <35466861+kdorcey@users.noreply.github.com>
2018-11-15 20:52:24 -0600

    made a new gather tweets function that works properly with the graphs that are broken up by hours

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -42,7 +42,7 @@ class SearchesController < ApplicationController
       if @current_user != nil
         now = Date.today
         from_date = now - params[:time].to_i
-        total_count, graph_data = Searches.gather_tweets(searches_params[:search_term].to_s, searches_params[:search_user], from_date.to_s, now.to_s, params[:time].to_i)
+        total_count, graph_data = Searches.new_gather_tweets(searches_params[:search_term].to_s, searches_params[:search_user], from_date.to_s, now.to_s, params[:time].to_i)
         search_hash = {}
         search_hash[:user_id] = @current_user.id
         search_hash[:search_term] = searches_params[:search_term].to_s
