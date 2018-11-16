6322704b4d0c30d5487d4e630273c63c790bd3c7
Kyle <35466861+kdorcey@users.noreply.github.com>
2018-11-16 14:01:40 -0600

    finished happpy paths for format_Date_holder

--- spec/models/searches_spec.rb
+++ spec/models/searches_spec.rb
@@ -8,12 +8,24 @@ describe Searches do
   end
 
   describe 'formatting date holder' do
-    it 'should properly format the date_vals hash' do
-      #expect(Searches).to receive(:gather_tweets).with('Ruby', '2018-10-10', '2018-10-25')
+    it 'should properly format date_vals for searches larger than 2 days' do
       ret = {"2018-10-12"=> 0, "2018-10-13"=>0, "2018-10-14"=>0}
       expect(Searches.format_date_holder("2018-10-12","2018-10-14",3)).
         to eq(ret)
     end
+    it 'should properly format date_vals for 2 day searches' do
+      ret = {"2018-10-12 00:00:00"=>0, "2018-10-12 12:00:00"=>0,
+             "2018-10-13 00:00:00"=>0, "2018-10-13 12:00:00"=>0}
+      expect(Searches.format_date_holder("2018-10-12","2018-10-13",2)).
+        to eq(ret)
+    end
+    it 'should properly format date_vals for 1 day searches' do
+      ret = {"2018-10-12 00:00:00"=>0, "2018-10-12 12:00:00"=>0,
+             "2018-10-12 18:00:00"=>0, "2018-10-12 23:59:00"=>0}
+      expect(Searches.format_date_holder("2018-10-12","2018-10-12",1)).
+        to eq(ret)
+    end
+
   end
 
   
