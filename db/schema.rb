99e9b6e9e4f479a98d3eca8c3a9273cb9b57d806
kylelevy <kyle.robert.levy@gmail.com>
2018-11-18 13:34:04 -0600

    Added twitter_handle to searches and updated search display

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181105223550) do
+ActiveRecord::Schema.define(version: 20181118191133) do
 
   create_table "searches", force: :cascade do |t|
     t.integer  "user_id"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20181105223550) do
     t.datetime "updated_at"
     t.boolean  "saved"
     t.string   "country"
+    t.string   "twitter_handle"
   end
 
   create_table "users", force: :cascade do |t|
