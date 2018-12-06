8a0344e53d6ec4ad04f9b83f4c38c16503ac8a62
kylelevy <kyle.robert.levy@gmail.com>
2018-12-05 21:04:06 -0600

    Multiple twitter users setup started

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -17,9 +17,9 @@ class SearchesController < ApplicationController
     if !@current_user.nil?
       @user_searches = Search.get_searches(@current_user)
     end
-     # @top_searches = Searches.where(country: @current_user.country).group(:search_term).order('count(*) DESC').limit(10)
-     # @top_searches = Searches.where(country: @current_user.country).group(:search_term).limit(10)
-     # @top_searches = Searches.where(country: @current_user.country).limit(10)
+    # @top_searches = Searches.where(country: @current_user.country).group(:search_term).order('count(*) DESC').limit(10)
+    # @top_searches = Searches.where(country: @current_user.country).group(:search_term).limit(10)
+    # @top_searches = Searches.where(country: @current_user.country).limit(10)
 
   end
 
@@ -37,7 +37,7 @@ class SearchesController < ApplicationController
 
       redirect_to root_path #Todo:: change this back to user path whene the JS is working properly.
 
-     # redirect_to user_path(@current_user.id)
+      # redirect_to user_path(@current_user.id)
     else
       flash[:notice] = "You are not logged in"
       redirect_to searches_path
@@ -47,7 +47,12 @@ class SearchesController < ApplicationController
   def create
     # Not Finished - Need to implement month end cases
 
-
+    all_twitter_handles = [searches_params[:search_user]]
+    params['searches'].keys.each do |field_name|
+      if field_name.include? "extra_handle"
+        all_twitter_handles.push params['searches'][field_name]
+      end
+    end
 
 
     if searches_params[:search_term].to_s.blank?
@@ -57,18 +62,27 @@ class SearchesController < ApplicationController
       if @current_user != nil
         now = Date.today
         from_date = now - params[:time].to_i
-        total_count, graph_data = Search.gather_tweets(searches_params[:search_term].to_s, searches_params[:search_user], from_date.to_s, now.to_s, params[:time].to_i)
 
-        #All of the information of the search stored into a hash
-        search_hash = {user_id: @current_user.id,search_term: searches_params[:search_term].to_s,
-                       twitter_handle: searches_params[:search_user].to_s, from_date: from_date,
-                        to_date: now, number_of_tweets: total_count, graph_data: graph_data}
+        #Used to store all the search hashes for each twitter handle
+        all_hashes = Array.new
+
+        #For every twitter handle entry, gather all of the tweets and put the data into the search hash
+        all_twitter_handles.each do |handle|
+          total_count, graph_data = Search.gather_tweets(searches_params[:search_term].to_s, handle, from_date.to_s, now.to_s, params[:time].to_i)
+
+          #All of the information of the search stored into a hash
+          search_hash = {user_id: @current_user.id, search_term: searches_params[:search_term].to_s,
+                         twitter_handle: searches_params[:search_user].to_s, from_date: from_date,
+                         to_date: now, number_of_tweets: total_count, graph_data: graph_data}
+
+          all_hashes.push search_hash
+        end
 
 
         new_search = Search.create_search!(search_hash)
 
 
-        @current_user.current_search=new_search.id #Set users current search to the search they just made
+        @current_user.current_search = new_search.id #Set users current search to the search they just made
         @current_user.save
 
         redirect_to searches_display_path
@@ -78,19 +92,19 @@ class SearchesController < ApplicationController
       end
     end
 
-  def destroy
+    def destroy
 
-  end
-
-  def display
-    if !@current_user.current_search.nil?
-    @curr_view_search = Search.find_by_id(@current_user.current_search)
-    else
-      flash[:notice] = "Hmm - Looks like you don't have any search..."
     end
-   # @user_searches = Searches.update_table
-  end
 
-end
+    def display
+      if !@current_user.current_search.nil?
+        @curr_view_search = Search.find_by_id(@current_user.current_search)
+      else
+        flash[:notice] = "Hmm - Looks like you don't have any search..."
+      end
+      # @user_searches = Searches.update_table
+    end
+
+  end
 end
 
