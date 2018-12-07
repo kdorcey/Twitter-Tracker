dc8f38b9a28483ee84ce101cca8c4e373ef3571f
Kyle <35466861+kdorcey@users.noreply.github.com>
2018-12-06 22:58:08 -0600

    deleted old create method

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -95,72 +95,19 @@ class SearchesController < ApplicationController
   end
 
 
-  def old_create
-    # Not Finished - Need to implement month end cases
-
-    all_twitter_handles = [searches_params[:search_user]]
-    params['searches'].keys.each do |field_name|
-      if field_name.include? "extra_handle"
-        all_twitter_handles.push params['searches'][field_name]
-      end
-    end
-
-
-    if searches_params[:search_term].to_s.blank?
-      flash[:notice] = "Please enter a search term"
-      redirect_to searches_path
-    else
-      new_search = nil
-      if @current_user != nil
-        now = Date.today
-        from_date = now - params[:time].to_i
-
-        #Used to store all the search hashes for each twitter handle
-        all_hashes = Array.new
-
-        #For every twitter handle entry, gather all of the tweets and put the data into the search hash
-        search_hash= {}
-        all_twitter_handles.each do |handle|
-          total_count, graph_data = Search.gather_tweets(searches_params[:search_term].to_s, handle, from_date.to_s, now.to_s, params[:time].to_i)
-
-          #All of the information of the search stored into a hash
-          search_hash = {user_id: @current_user.id, search_term: searches_params[:search_term].to_s, from_date: from_date,
-                         to_date: now, number_of_tweets: total_count, graph_data: graph_data}
-
-          all_hashes.push search_hash
-
-        end
-        new_search = Search.create_search!(search_hash, all_twitter_handles)
-        new_search.view_count=0
-        new_search.save!
-
-        temp = []
-        new_search.twitterhandle do |test|
-          temp << test
-          puts "wow"
-          puts test
-        end
-        puts "woof"
-        puts temp
-
-        @current_user.current_search = new_search.id #Set users current search to the search they just made
-        @current_user.save
-        
-        redirect_to searches_display_path
-      else
-        flash[:notice] = "Nah homie, gotta make an account first."
-        redirect_to root_path
-      end
-    end
-
     def destroy
 
     end
 
     def display
       if !@current_user.current_search.nil?
-        
-        @curr_view_search = Search.find_by_id(@current_user.current_search)
+        #@curr_view_search = Search.find_by_id(@current_user.current_search)
+
+        Search.where(id: @current_user.current_search).
+
+
+        @current_view_search
+
       else
         flash[:notice] = "Hmm - Looks like you don't have any search..."
       end
